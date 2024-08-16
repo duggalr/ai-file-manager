67e4073bd055edb361c0fc11ca41576c61b31030
duggalr <duggalr42@gmail.com>
2024-08-16 17:13:41 -0400

    prompts

--- backend/scripts/file_process/mp_main_two.py
+++ backend/scripts/file_process/mp_main_two.py
@@ -35,11 +35,12 @@ from . import process_directory_main_two
 class Prompts(Enum):
     """
     """
-    CATEGORIZATION_PROMPT_V1 = """You task is to specialize in file categorization. I have provided you with a screenshot of a file. Your task is to analyze the content and context within the screenshot and categorize the file into an appropriate entity type, a primary category, and relevant sub-categories. Your categorization should be comprehensive and accurate, capturing the essence of the file's content.
+    CATEGORIZATION_PROMPT_V1 = """You task is to specialize in file categorization. I have provided you with a screenshot of a file. Your task is to analyze the content and context within the screenshot and categorize the file into an appropriate entity type, a primary category, and relevant sub-categories. Your categorization should be comprehensive and accurate, capturing the essence of the file's content. Also, generate a semantically appropriate, human-readble name for the file, based on the screenshot. The name you generate is meant to be shown on the website, for the user to easily identify the file.
 
 Please structure your response in the following JSON format:
 
 {
+    "generated_file_name": "...",
     "entity_type": "Specify the type of the file (e.g., Book, Paper, Image, Invoice, Spreadsheet, etc.)",
     "primary_category": "Name of the main category that best describes the file",
     "sub_categories": [
@@ -50,6 +51,7 @@ Please structure your response in the following JSON format:
 }
 
 Guidelines:
+- **File Name:**: The semantic name of the file based on the screenshot.
 - **Entity Type**: Identify the most accurate file type based on the content visible in the screenshot.
 - **Primary Category**: Determine the overarching category that encapsulates the file’s primary purpose or theme.
 - **Sub-categories/Tags**: List a few additional categories or tags that provide further context or details about the file's content.
