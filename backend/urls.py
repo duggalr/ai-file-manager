0544eebc9dfdaf52f8514cf1472a31f8d1f3592f
duggalr <duggalr42@gmail.com>
2024-08-21 16:21:33 -0400

    updating landing

--- backend/urls.py
+++ backend/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('blog/welcome', views.blog_post_one, name='blog_post_one'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def landing(request):
     return render(request, 'validation/landing.html')
+
+
+def blog_post_one(request):
+    # TODO: after blog post is created, register the url based on the blog content and proceed from there
+    return render(request, 'validation/blog_post_one.html')
