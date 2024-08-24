12be32e5c5230f100e635aab71049742f3be2e7f
duggalr <duggalr42@gmail.com>
2024-08-24 15:59:55 -0400

    google analytics

--- ai-file-manager-frontend/src/pages/_document.js
+++ ai-file-manager-frontend/src/pages/_document.js
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
  
 export default function Document() {
 
@@ -18,8 +20,23 @@ export default function Document() {
                     window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
                     heap.load("${heapEnvId}");
                     `,
-                }}
+                }}                
                 />
+
+                <Script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-6N4PD5VHJ0`}
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-6N4PD5VHJ0');
+                    `}
+                </Script>
+                    
             </Head>
             <body>
                 <Main />
